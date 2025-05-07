e784827498a36ebb951f8bb00d0c83e9c0de2ca7
juan-antonio <juanantoniomtzfns@gmail.com>
2025-05-07 13:38:36 +0200

    feat(client): add ProductClient to consume mock APIs

---
+++ similar-products/src/main/java/com/test/similarproducts/client/ProductClient.java
@@ -0,0 +1,44 @@
+package com.test.similarproducts.client;
+
+import com.test.similarproducts.model.ProductDetail;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+
+import java.util.Collections;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class ProductClient {
+
+    private final WebClient webClient = WebClient.builder()
+            .baseUrl("http://localhost:3001")
+            .build();
+
+    public List<String> getSimilarProductIds(String productId) {
+        try {
+            return webClient.get()
+                    .uri("/product/{id}/similarids", productId)
+                    .retrieve()
+                    .bodyToFlux(String.class)
+                    .collectList()
+                    .block();
+        } catch (WebClientResponseException.NotFound e) {
+            return Collections.emptyList();
+        }
+    }
+
+    public ProductDetail getProductDetail(String productId) {
+        try {
+            return webClient.get()
+                    .uri("/product/{id}", productId)
+                    .retrieve()
+                    .bodyToMono(ProductDetail.class)
+                    .block();
+        } catch (WebClientResponseException.NotFound e) {
+            return null;
+        }
+    }
+}
