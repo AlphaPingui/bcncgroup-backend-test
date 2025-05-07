56e9bcd4fb43b4a375794e8b081630354b67b5c0
juan-antonio <juanantoniomtzfns@gmail.com>
2025-05-07 13:17:04 +0200

    chore: add Project to repo

---
+++ similar-products/build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.4.5"
+	id("io.spring.dependency-management") version "1.1.7"
+	id("com.diffplug.spotless") version "6.25.0"
+	id("checkstyle")
+}
+
+group = "com.test"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	compileOnly("org.projectlombok:lombok")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+spotless {
+	java {
+		target("src/**/*.java")
+		googleJavaFormat() // Estilo oficial de Google
+		trimTrailingWhitespace()
+		endWithNewline()
+		removeUnusedImports()
+	}
+}
+
+checkstyle {
+	toolVersion = "10.12.1"
+	configDirectory.set(file("config/checkstyle"))
+}
---
+++ similar-products/src/test/java/com/test/similarproducts/SimilarProductsApplicationTests.java
@@ -0,0 +1,11 @@
+package com.test.similarproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimilarProductsApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
