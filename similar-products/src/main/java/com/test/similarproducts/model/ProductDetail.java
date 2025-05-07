8820372d57adf97e474ccb4ab50dc4dfa5c11bf5
juan-antonio <juanantoniomtzfns@gmail.com>
2025-05-07 13:21:45 +0200

    feat(model): add ProductDetail model based on contract

---
+++ similar-products/src/main/java/com/test/similarproducts/model/ProductDetail.java
@@ -0,0 +1,15 @@
+package com.test.similarproducts.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDetail {
+    private String id;
+    private String name;
+    private double price;
+    private boolean availability;
+}
